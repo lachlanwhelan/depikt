82ed2ee17bb5916e5417df4f5875c9d589d18645
Lachlan Whelan <lachlan.whelan1@gmail.com>
2020-01-28 14:08:41 +1100

    Updates

---
+++ precache-manifest.784872315aba2c435af9c5472ceea2ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19504ac63fcc559085ae05cd7adec4ac",
+    "url": "/depikt/index.html"
+  },
+  {
+    "revision": "02ca7bef2918474e6161",
+    "url": "/depikt/static/css/main.2860a69e.chunk.css"
+  },
+  {
+    "revision": "f6b83c00686d5f99c989",
+    "url": "/depikt/static/js/2.9673b26c.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/depikt/static/js/2.9673b26c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "02ca7bef2918474e6161",
+    "url": "/depikt/static/js/main.c1745d54.chunk.js"
+  },
+  {
+    "revision": "3b3105b88db2959d46c8",
+    "url": "/depikt/static/js/runtime-main.6b52a9e2.js"
+  }
+]);
